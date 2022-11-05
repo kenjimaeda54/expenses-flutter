da801e9e2d0db3e15bfaefea2b0fb4e7af86fc57
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-04 23:08:29 -0300

    feat: styleing card list transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -9,12 +10,27 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return  MaterialApp(home: HomeScreen());
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  final _transactions =[
+    Transaction(
+        id: "t1",
+        title: "New running shoes",
+        value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+        id: "t2",
+        title: "Electricity bill",
+        value: 211.30,
+        date: DateTime.now()
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +38,70 @@ class HomeScreen extends StatelessWidget {
        appBar: AppBar(
          title: const  Text("Personal expenses"),
        ),
-      body: const Center(
-        child:  Text("Version initial"),
+      body:  Column(
+         //mesmo conceito do flex box
+        //se estamos no eixo de column entao o cross eixo alinha horizontal
+        //e o mainAxis alinha em coluna
+        //os comportamentos sao identicos ao flex box, exemplo stretch vai espichar
+        // ao maximo o filho
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children:   [
+          const SizedBox(
+             child:   Card(
+               elevation: 5,
+               color: Colors.blue,
+               child: Text("Graphic"),
+             ),
+           ),
+           Column(
+             children: _transactions.map((it) => Card(
+                  child: Row(
+                     children: [
+                         Container(
+                           margin: const EdgeInsets.symmetric(
+                             horizontal: 12,
+                             vertical: 10,
+                           ),
+                           padding: const EdgeInsets.all(10),
+                           decoration: BoxDecoration(
+                             border: Border.all(
+                               color: Colors.purple,
+                               width: 2
+                             )
+                           ),
+                           child: Text(
+                               it.value.toString(),
+                               style: const TextStyle(
+                                 fontWeight: FontWeight.bold,
+                                 fontSize: 20,
+                                 color: Colors.purple
+                               ),
+                           ),
+                         ),
+                         Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                              Text(
+                                  it.title,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                              ),
+                              Text(
+                                  it.date.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.grey
+                                  )
+                              )
+                           ],
+                         )
+                     ],
+                  ),
+             )).toList(),
+           )
+         ],
       ),
     );
   }
